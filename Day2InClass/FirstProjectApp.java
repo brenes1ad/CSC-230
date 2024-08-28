16660185dc930d5ca8770d710589d0eedc4875ac
brenes1ad <adbrenes05@gmail.com>
2024-08-28 14:40:45 -0400

    Day 2 In Class Work

---
+++ Day2InClass/FirstProjectApp.java
@@ -0,0 +1,28 @@
+package Day2InClass;
+import java.util.Random;
+
+
+
+public class FirstProjectApp {
+    /**
+     *
+     * @param args -- command line argument
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome");
+
+        int i;
+        for (i = 1; i < 6; i++)
+            System.out.println("i= " + i + " and sqrt(i) = " + Math.sqrt(i));
+
+        System.out.println("Int Max is  " + Integer.MAX_VALUE);
+        System.out.println("rand = " + Math.random());
+        System.out.println("rand2 = " + Math.random());
+        System.out.println("rand3 = " + Math.random());
+
+        Random rand = new Random();
+
+        Dog fido = new Dog("Lucas", 5, "Mutt");
+        System.out.println("\n" + fido);
+    }
+}
