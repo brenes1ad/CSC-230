ae28d7e1ae7f8c24441a077d1145b454df7c1f06
brenes1ad <adbrenes05@gmail.com>
2024-10-09 21:37:45 -0400

    Assignment Finished

--- DiceBrenes/Die.java
+++ DiceBrenes/Die.java
@@ -2,6 +2,11 @@ package DiceBrenes;
 
 /**
  * Represents one die (singular of dice) with faces showing between 1 and max value
+ * -----------------------------
+ * Everything in this class has default protection(package protection) because I feel like
+ * now that a handful class is written and functioning, there is no reason to ever use the die class on its own.
+ * All the methods are used by the handful class so they can't have private status and that also means this
+ * can't just be an abstract class for a handful, since the handful is a collection of dice objects.
  */
 public class Die {
     private final int MAX;  // maximum face value
@@ -10,17 +15,12 @@ public class Die {
     /**
      * @param max number of faces on the die. Must be at least 4; otherwise a 6-sided die is created.
      */
-    public Die(int max) {
+     Die(int max) {
         if (max >= 4) {
             MAX = max;
         } else {
             MAX = 6;
         }
-        roll();
-    }
-
-    public Die(){
-        MAX = 6;
         roll();
     }
 
@@ -31,7 +31,7 @@ public class Die {
      *
      * @return random value between 1 and max face value of die.
      */
-    public int roll () {
+     int roll () {
         faceValue = (int) (Math.random() * MAX) + 1;
         return faceValue;
     }
@@ -41,11 +41,17 @@ public class Die {
     }
 
     /**
+     *
+     * @return face value of die
+     */
+     int getFaceValue() {return faceValue;}
+
+    /**
      * main() tests a single die to make sure it works
      * @param args -- null argument list ignored
      */
     public static void main(String[] args) {
-        Die die1 = new Die();
+        Die die1 = new Die(6);
         System.out.println(die1);
     }
 }
--- DiceBrenes/Handful.java
+++ DiceBrenes/Handful.java
@@ -1,18 +1,40 @@
 package DiceBrenes;
-
 import java.util.ArrayList;
 
+/**
+ * Represents a handful of dice.
+ * ------------
+ * Doesn't use inheritance or an interface because I don't feel like it needs it. This is a has-a relationship,
+ * a handful has a die(many) which doesn't call for parent child relationships of the dice and handfuls. I could
+ * see potentially using an interface since they both share roll and sum methods, but that seems to excess for
+ * two methods, especially since the die class kind of acts as a driver for the handful and after you make the
+ * handful class, you really don't need to make a singular die anymore.
+ */
 public class Handful {
     private ArrayList<Die> dice = new ArrayList<>();
     private final int FACES;
 
+    /**
+     *  The ArrayList and final int FACES are both set to private because neither of those should be accessed
+     *  outside the class, they really shouldn't be accessed outside the couple methods written that use them.
+     * @param number amount of dice wanted in handful
+     * @param faces size of the dice, number of faces each die has
+     */
     public Handful(int number, int faces){
         FACES = faces;
         for(int i = 0; i < number; i++){
             dice.add(new Die(faces));
         }
     }
-    public void rollHandfull(){
+
+    /**
+     * Rolls each die in the handful and prints out the number of the die and its rolled value.
+     * -----------------------------------------------------------
+     * Without knowing the full intended use of this class and methods, I feel like it's practical to keep
+     * the rolling method public so anyone/anywhere can roll and handful that can also be created by
+     * anyone/anywhere. This is the primary function of the handful so it should be public.
+     */
+    public void rollHandful(){
         int counter = 1;
         System.out.println("Rolling " + dice.size() + "d" + FACES);
         System.out.println("-------------------");
@@ -23,8 +45,29 @@ public class Handful {
         }
     }
 
+    /**
+     * Same as the roll method, without knowing where and how this class would be used, it makes sense
+     * that anyone/anywhere can get the sum of all the dice. Since this changes nothing about any values of
+     * the dice or the handful, I wouldn't ever see a reason that it couldn't be public.
+     *
+     * @return sum of each face value of the dice in the handful.
+     */
+    public int sum(){
+        int total = 0;
+        for(Die d : dice) {
+            total += d.getFaceValue();
+        }
+        return total;
+    }
+
+    /**
+     * Simple class used to create handful, test the rolling method for actually rolling and for printing
+     * accuracy and tests the sum method to insure it sums all the dice correctly.
+     * @param args default parameters for main method
+     */
     public static void main(String[] args){
-        Handful handful = new Handful(96, 6);
-        handful.rollHandfull();
+        Handful handful = new Handful(6, 6);
+        handful.rollHandful();
+        System.out.println(handful.sum());
     }
 }
